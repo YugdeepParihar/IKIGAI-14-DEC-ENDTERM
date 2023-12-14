9a3159adf27af46046c4281f5a44f2a829a92d56
Yugdeep Parihar <106156641+YugdeepParihar@users.noreply.github.com>
2023-12-14 15:39:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,18 @@ const api = {
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
-function setQuery(evt) {
-  if (evt.keyCode == 13) {
-    getResults(searchbox.value);
-  }
-}
-
 function getResults (query) {
   fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
     .then(weather => {
       return weather.json();
     }).then(displayResults);
+}
+
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
 }
 
 function displayResults (weather) {
@@ -39,7 +40,7 @@ function displayResults (weather) {
 
 function dateBuilder (d) {
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ,"Sunday"];
 
   let day = days[d.getDay()];
   let date = d.getDate();
